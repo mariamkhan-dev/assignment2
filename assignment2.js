c1be40322a9842b5b00e00def74c77a2affe408e
Mariam Khan <67177443+mariamkhan-dev@users.noreply.github.com>
2021-09-27 22:11:58 -0400

    adding given Push and forEach implementations

--- assignment2.js
+++ assignment2.js
@@ -1,6 +1,9 @@
 // FOR EACH //
-Array.prototype.myEach = function() {
-
+Array.prototype.myEach = function(callbackFn) { 	
+  for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined) continue;
+      callbackFn(this[i], i, this);
+    }
 };
 
 // MAP //
@@ -39,8 +42,15 @@ Array.prototype.myIndexOf = function() {
 };
 
 // PUSH //
-Array.prototype.myPush = function() {
-
+Array.prototype.myPush = function(...args) {
+  let arg_i = 0;
+	let length = this. length;
+  
+	for(let i = length; i < length + args.length; i++) {
+		this[i] = args[arg_i];
+		arg_i++;
+	}
+	return this.length;
 };
 
 // LASTINDEXOF //
